0adc8f84d6bbcf7540dc04366c5131fed9661344
Gustavo Ferreira <53050800+Gustavowp8@users.noreply.github.com>
2022-01-07 11:31:26 -0300

    Add files via upload

---
+++ estilo/app.js
@@ -0,0 +1,9 @@
+function chamaGus() {
+    alert('Você sera direcionado para outro site/app')
+    location='https://api.whatsapp.com/send?phone=5561995084318&text=Ol%C3%A1%20Gustavo%2C%20'
+}
+
+myvideo = document.getElementById('videoM');
+myvideo.autoplay = true;
+myvideo.loop = true;
+myvideo.load();
