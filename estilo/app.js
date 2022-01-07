07596a830704262f2d973e043cd42f24e0d1d6a1
Gustavo Ferreira <53050800+Gustavowp8@users.noreply.github.com>
2022-01-07 13:57:01 -0300

    Delete estilo directory

--- estilo/app.js
+++
@@ -1,9 +0,0 @@
-function chamaGus() {
-    alert('Você sera direcionado para outro site/app')
-    location='https://api.whatsapp.com/send?phone=5561995084318&text=Ol%C3%A1%20Gustavo%2C%20'
-}
-
-myvideo = document.getElementById('videoM');
-myvideo.autoplay = true;
-myvideo.loop = true;
-myvideo.load();
